616734d4b100949b234e2d8475cf5e2e430b88d6
Arjav Jain <arjavjain0703@gmail.com>
2025-10-08 22:08:04 +0530

    refactor

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,11 @@
 use axum::{
     Router,
-    extract::{Json, State},
     routing::{get, post},
 };
-use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
-use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 use std::net::SocketAddr;
 use std::sync::Arc;
-
-#[derive(Debug, Deserialize)]
-struct CreateUserRequest {
-    username: String,
-    email: String,
-    password: String,
-}
-
-#[derive(Debug, Serialize)]
-struct ApiResponse {
-    status: String,
-    message: String,
-}
+mod user_operations;
+use user_operations::{connect_to_database, create_user};
 
 #[tokio::main]
 async fn main() {
@@ -45,68 +30,4 @@ async fn main() {
 
 async fn hello_world() -> &'static str {
     "Hello, World!"
-}
-
-async fn connect_to_database() -> Pool<Postgres> {
-    println!("Connecting to the database...");
-
-    let db_url = "postgres://arjav:arjav@localhost/user_db";
-    dbg!(&db_url);
-
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(db_url)
-        .await
-        .expect("Failed to create pool.");
-
-    let _ = sqlx::query(
-        "CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(100) NOT NULL UNIQUE,
-            password_hash VARCHAR(256) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )",
-    )
-    .execute(&pool)
-    .await;
-
-    println!("Connected to the database.");
-    pool
-}
-
-async fn create_user(
-    State(pool): State<Arc<Pool<Postgres>>>,
-    Json(payload): Json<CreateUserRequest>,
-) -> Json<ApiResponse> {
-    let password_hash = hash_password(&payload.password);
-
-    let query_result =
-        sqlx::query("INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3)")
-            .bind(payload.username)
-            .bind(payload.email)
-            .bind(password_hash)
-            .execute(&*pool)
-            .await;
-
-    match query_result {
-        Ok(_) => Json(ApiResponse {
-            status: "success".to_string(),
-            message: "User created successfully".to_string(),
-        }),
-        Err(e) => {
-            eprintln!("Database error: {:?}", e);
-            Json(ApiResponse {
-                status: "error".to_string(),
-                message: format!("Failed to create user: {}", e),
-            })
-        }
-    }
-}
-
-fn hash_password(password: &str) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(password.as_bytes());
-    let result = hasher.finalize();
-    format!("{:x}", result)
 }
